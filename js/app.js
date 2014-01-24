72a73e0026884cefa5f07ece78df39d5fd2aa7ad
N.Shimizu <chikoski@gmail.com>
2014-01-25 07:51:40 +0900

    changed the id of the textarea, which provides the functionality to write descriptions, to description. set app to save all modifications when .next buttons are clicked

--- js/app.js
+++ js/app.js
@@ -23,19 +23,48 @@ requirejs.config({
 require(["lib/bootstrap", "lib/jquery", "main"], function(Bootstrap, $, App){
 	var $steps = $(".step");
 	var scrollbar = $("html,body");
-
 	var app = new App();
+
+
+	var saveAllModifications = function(){
+		app.editEndpoint(elms.endpoint.val());
+		app.editSPARQL(elms.sparql.val());
+		app.editDescription(elms.description.val());
+	};
+	
 	app.restore();
 	$steps.height($(window).height());
 
+	var elms = {
+		endpoint: $("#endpoint"),
+		sparql: $("#sparql"),
+		description: $("#description"),
+		result: $("#result")
+	};
+	
+	elms.endpoint.change(function(event){
+		app.editEndpoint(elms.endpoint.val());
+	});
+
+	elms.sparql.change(function(event){
+		app.editSPARQL(elms.sparql.val());
+	});
+
+	elms.description.change(function(event){
+		app.editDescription(elms.description.val());
+	});
+
+
 	$(".next").click(function(event){
 		event.preventDefault();
+		saveAllModifications();
 		var next_step = $(this).parents(".step").next();
 		scrollbar.animate({scrollTop: next_step.offset().top});
 	});
 
 	$(".finish").click(function(event){
 		event.preventDefault();
+		saveAllModifications();
 		app.finishEditting();
 		app.newSnipet();
 		scrollbar.animate({scrollTop: 0});
